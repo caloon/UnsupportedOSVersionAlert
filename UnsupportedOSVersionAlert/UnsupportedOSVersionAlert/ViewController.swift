8be1b5fd4b4fe02eba4a97e6c8d9c61b1d2046f1
Josef <Josef@Josefs-MacBook.fritz.box>
2016-06-30 11:50:54 +0200

    Project setup

---
+++ UnsupportedOSVersionAlert/UnsupportedOSVersionAlert/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UnsupportedOSVersionAlert
+//
+//  Created by Josef Moser on 30/06/2016.
+//  Copyright © 2016 Josef Moser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, OSVersionCheckerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
