98cdff321a7d38c853db5a39d230d11be93fe29f
Josef <Josef@Josefs-MacBook.fritz.box>
2016-06-30 11:57:08 +0200

    Defined OSVersionCheckerDelegate

---
+++ UnsupportedOSVersionAlert/UnsupportedOSVersionAlert.swift
@@ -0,0 +1,22 @@
+//
+//  UnsupportedOSVersionAlert.swift
+//  UnsupportedOSVersionAlert
+//
+//  Created by Josef Moser on 30/06/2016.
+//  Copyright © 2016 Josef Moser. All rights reserved.
+//
+
+import UIKit
+
+protocol OSVersionCheckerDelegate {
+    func didCheckOSVersion(supported: Bool!)
+}
+
+class OSVersionChecker: NSObject {
+
+}
+
+class OSVersionAlert: NSObject {
+    
+}
+
